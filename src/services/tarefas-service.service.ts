ee5ecb5e4450cecadf01cbc75ab96203fc60e98e
gleiMoura <gle.bar.mou@gmail.com>
2023-11-19 13:53:09 -0300

    ✨ Criate all methods in service

--- src/services/tarefas-service.service.ts
+++ src/services/tarefas-service.service.ts
@@ -2,6 +2,13 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+interface TarefaType {
+  nome: string,
+  proprietario: string,
+  dataInicio: string,
+  dataTermino: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,8 +17,27 @@ export class TarefasService {
 
   constructor(private httpClient: HttpClient) { };
 
+  //cria um observable para pegar as tarefas existentes da API
   obterTarefas(): Observable<any[]> {
     return this.httpClient.get<any>(this.apiUrl + '/pegarTarefas')
   }
-}
 
+  //Cria um observable para pegar as tarefas com base no ID
+  obterTarefasPorId(id: number): Observable<any[]> {
+    return this.httpClient.get<any>(`${this.apiUrl}/pegarTarefas/${id}`);
+  }
+
+  //obtem as tarefas passando como propriedade o nome do proprietário
+  obterTarefasPorProprietario(proprietario: string): Observable<any[]> {
+    return this.httpClient.get<any[]>(`${this.apiUrl}/pegarTarefas/${proprietario}`);
+  };
+
+  //envia a tarefa para a API
+  criarTarefa(novaTarefa: TarefaType): Observable<any[]> {
+    return this.httpClient.post<any>(this.apiUrl + "/criarTarefa", novaTarefa);
+  }
+
+  editarTarefa(id: number, tarefa: TarefaType): Observable<any[]> {
+    return this.httpClient.put<any>(`${this.apiUrl}/editarTarefa/${id}`, tarefa);
+  }
+}
