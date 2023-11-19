6ef795a1e857e3d8194cf2a8f013d0e3f1f864ef
gleiMoura <gle.bar.mou@gmail.com>
2023-11-19 14:10:37 -0300

    ✨ Creates a function to send task to API

--- src/components/criador-de-tarefas/criador-de-tarefas.component.ts
+++ src/components/criador-de-tarefas/criador-de-tarefas.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TarefasService } from '../../services/tarefas-service.service';
 
 @Component({
   selector: 'criador-de-tarefas',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './criador-de-tarefas.component.html'
 })
 export class CriadorDeTarefasComponent {
@@ -14,10 +16,34 @@ export class CriadorDeTarefasComponent {
     proprietario: '',
     dataInicio: '',
     dataTermino: '',
-    corDaTarefa:''
+    corDaTarefa: ''
   };
 
+  constructor(
+    public tarefasService: TarefasService
+  ) { }
 
+
+  criarTarefa() {
+    this.tarefasService.criarTarefa(this.novaTarefa).subscribe(
+      (resposta) => {
+        alert('Tarefa Criada com Sucesso!');
+        console.log(resposta);
+
+        //limpa os dados da novaTarefa, que recebe eles dos imputs
+        this.novaTarefa = {
+          nome: '',
+          proprietario: '',
+          dataInicio: '',
+          dataTermino: '',
+          corDaTarefa: ''
+        };
+      },
+      (erro) => {
+        console.error('Erro ao criar tarefa', erro)
+      }
+    )
+  }
 }
 
 //Aqui estamos criando uma nova tarefa através da propriedade TWO-WAY DATA BINDING. Quando importamos o FORMSMODULE
